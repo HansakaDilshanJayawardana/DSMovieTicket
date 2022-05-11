e4420295baa8f600e593b0b0553690a856f884e7
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-12 04:09:56 +0530

    Movie Types Class

---
+++ src/main/java/com/app/dsmovieticket/utility/MovieTypes.java
@@ -0,0 +1,11 @@
+package com.app.dsmovieticket.utility;
+
+public class MovieTypes {
+
+    public static String SINHALA = "SINHALA";
+    public static String ENGLISH = "ENGLISH";
+    public static String HINDI = "HINDI";
+    public static String TAMIL = "TAMIL";
+    public static String TELUGU = "TELUGU";
+
+}

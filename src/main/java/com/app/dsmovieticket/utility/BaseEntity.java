33650dca98a2b7bd5e2aac5b33b8cceccd212c75
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-11 13:58:14 +0530

    Base Entity Class

---
+++ src/main/java/com/app/dsmovieticket/utility/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.app.dsmovieticket.utility;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@Setter
+@Getter
+public class BaseEntity {
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private Long createdBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private Long modifiedBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modified_at")
+    private Date modifiedAt;
+
+}

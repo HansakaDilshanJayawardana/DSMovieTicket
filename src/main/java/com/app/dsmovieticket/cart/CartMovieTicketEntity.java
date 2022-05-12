efbd73e669217b60cef54708d329a7df2d097c76
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-13 00:20:39 +0530

    Cart Movie Ticket Entity Class

---
+++ src/main/java/com/app/dsmovieticket/cart/CartMovieTicketEntity.java
@@ -0,0 +1,36 @@
+package com.app.dsmovieticket.cart;
+
+import com.app.dsmovieticket.movie.MovieEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_cart_movie_tickets")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CartMovieTicketEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_cart_id")
+    @JsonIgnore
+    private ShoppingTicketCartEntity ticketCartId;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private MovieEntity movieId;
+
+    @Column(name = "no_of_tickets")
+    private int noOfTickets;
+
+}

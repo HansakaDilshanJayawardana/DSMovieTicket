cc5f08caf01de119dae3173ede4c9540ca733ec3
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-18 05:11:13 +0530

    Theatre Class

---
+++ src/main/java/com/app/dsmovieticket/utility/Theatre.java
@@ -0,0 +1,10 @@
+package com.app.dsmovieticket.utility;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Theatre {
+
+    public static ArrayList<String> theatre = new ArrayList<>(Arrays.asList("PVR LUXE ONE","PVR ONE","PVR PLAY HOUSE ONE"));
+
+}

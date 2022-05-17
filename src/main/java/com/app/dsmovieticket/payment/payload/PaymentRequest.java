e16ddcc2ebf52678da2b99518937db907966440b
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-18 05:02:11 +0530

    Payment Request Class

---
+++ src/main/java/com/app/dsmovieticket/payment/payload/PaymentRequest.java
@@ -0,0 +1,32 @@
+package com.app.dsmovieticket.payment.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentRequest {
+
+    @NonNull
+    private String paymentType;
+    @NonNull
+    private long cartId;
+    private String cardType;
+    private String cardNumber;
+    private String csv;
+    private String cardHolder;
+    private Date expDate;
+    private String phoneNumber;
+    @NonNull
+    private float amount;
+    //Theatre Options
+    @NonNull
+    private String theatreType;
+    private String theatreAddress;
+
+}

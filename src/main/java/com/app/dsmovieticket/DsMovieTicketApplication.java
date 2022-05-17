a99b88486c1675474f306801cf8d3e8c55d675c2
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-18 03:20:28 +0530

    Add RestTemplate Bean

--- src/main/java/com/app/dsmovieticket/DsMovieTicketApplication.java
+++ src/main/java/com/app/dsmovieticket/DsMovieTicketApplication.java
@@ -2,12 +2,20 @@ package com.app.dsmovieticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DsMovieTicketApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DsMovieTicketApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
 }

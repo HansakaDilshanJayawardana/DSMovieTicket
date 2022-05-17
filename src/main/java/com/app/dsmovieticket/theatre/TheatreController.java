30d96a9f7addba3ab16c9ecf231d6eb1a40da4be
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-18 05:11:55 +0530

    Theatre Controller Class

---
+++ src/main/java/com/app/dsmovieticket/theatre/TheatreController.java
@@ -0,0 +1,23 @@
+package com.app.dsmovieticket.theatre;
+
+import com.app.dsmovieticket.utility.Theatre;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/v1/theatre")
+public class TheatreController {
+
+    @GetMapping("/list")
+    public ResponseEntity<ArrayList<String>> getList(){
+        return new ResponseEntity<>(Theatre.theatre, HttpStatus.OK);
+    }
+
+}

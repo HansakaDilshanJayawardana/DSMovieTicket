ccb991ba42a6f0846f972df3fe1a650674fb4e3a
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-18 05:35:29 +0530

    Payment Type Class

---
+++ src/main/java/com/app/dsmovieticket/payment/PaymentType.java
@@ -0,0 +1,9 @@
+package com.app.dsmovieticket.payment;
+
+public class PaymentType {
+
+    public static String CARD = "CARD";
+    public static String MOBILE = "MOBILE";
+    private static String CASH = "CASH";
+
+}

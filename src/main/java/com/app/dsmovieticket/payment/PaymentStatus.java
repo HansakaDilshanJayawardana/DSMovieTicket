bb575744954303194db3a6ce2403b06f602a3300
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-05-18 05:32:23 +0530

    Payment Status Class

---
+++ src/main/java/com/app/dsmovieticket/payment/PaymentStatus.java
@@ -0,0 +1,9 @@
+package com.app.dsmovieticket.payment;
+
+public class PaymentStatus {
+
+    public static final String DONE = "DONE";
+    public static final String PENDING = "PENDING";
+    public static final String PENDINGFORCONFIRM = "PENDINGFORCONF";
+
+}
